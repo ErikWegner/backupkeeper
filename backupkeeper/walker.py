fd0eff5dd7baaa25d20cbbfe7578b0810c56a8df
Erik Wegner <E_Wegner@web.de>
2019-02-03 18:23:28 +0100

    Ignore directories not matching the date pattern

--- backupkeeper/walker.py
+++ backupkeeper/walker.py
@@ -1,4 +1,5 @@
 import os
+import re
 from .backupmetadata import BackupMetadata
 
 storage_file = ".backupkeepdate"
@@ -9,20 +10,24 @@ def walker(root_directory):
        Load existing backups
     """
     existing_backups = []
+    datepattern = re.compile('\\d{4}-\\d{2}-\\d{2}')
     with os.scandir(root_directory) as it:
         for entry in it:
-            if not entry.name.startswith('.') and entry.is_dir():
-                created_date = entry.name
-                storage_file_name = None
+            if entry.name.startswith('.') or not entry.is_dir():
+                continue
+            if datepattern.match(entry.name) is None:
+                continue
+            created_date = entry.name
+            storage_file_name = None
 
-                if os.path.isfile(os.path.join(entry.path, storage_file)):
-                    storage_file_name = os.path.join(entry.path, storage_file)
-                if os.path.isfile(os.path.join(entry.path, '.backupdatum')):
-                    storage_file_name = os.path.join(
-                        entry.path, '.backupdatum')
-                if storage_file_name:
-                    with open(storage_file_name) as f:
-                        keep_date = f.read()
-                        existing_backups.append(
-                            BackupMetadata(created_date, keep_date))
+            if os.path.isfile(os.path.join(entry.path, storage_file)):
+                storage_file_name = os.path.join(entry.path, storage_file)
+            if os.path.isfile(os.path.join(entry.path, '.backupdatum')):
+                storage_file_name = os.path.join(
+                    entry.path, '.backupdatum')
+            if storage_file_name:
+                with open(storage_file_name) as f:
+                    keep_date = f.read()
+                    existing_backups.append(
+                        BackupMetadata(created_date, keep_date))
     return existing_backups
--- backupkeeper/walker_test.py
+++ backupkeeper/walker_test.py
@@ -10,5 +10,5 @@ def test_reads():
 def test_reads_keep_dates():
     result = walker("test_backups")
 
-    assert result[0]._keep_date == "2019-05-11"
-    assert result[1]._keep_date == "2019-12-01"
+    assert result[0]._keep_date == "2019-05-11" or result[0]._keep_date == "2019-12-01"
+    assert result[1]._keep_date == "2019-12-01" or result[1]._keep_date == "2019-05-11"
